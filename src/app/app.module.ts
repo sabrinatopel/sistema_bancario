e3d1eeb1dd8faa91a4b527fa37e753d5d112758e
lab10maql42 <lab10maql42@debian-BULLSEYE-live-builder-AMD64>
2023-09-09 11:53:46 -0300

    add bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from 'src/shared/material/material-module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MaterialModule
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
