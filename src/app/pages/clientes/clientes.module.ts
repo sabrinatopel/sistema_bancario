ebeee7503e11806aae9b81d3fc3ccf150b884227
lab10maql42 <lab10maql42@debian-BULLSEYE-live-builder-AMD64>
2023-09-09 14:06:40 -0300

    add listagem-clientes

---
+++ src/app/pages/clientes/clientes.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ListagemClientesComponent } from './listagem-clientes/listagem-clientes.component';
+
+
+
+@NgModule({
+  declarations: [
+    ListagemClientesComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ClientesModule { }
